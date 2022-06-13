6a1eba3d4ce010dd7bae5dbc3875ad9d0cf8d3da
wang <329737745@qq.com>
2022-06-13 21:56:48 +0800

    first commit

---
+++ stream/src/main/java/com/robam/stream/fragment/HomeViewModel.java
@@ -0,0 +1,7 @@
+package com.robam.stream.fragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ ventilator/src/main/java/com/robam/ventilator/fragment/HomeViewModel.java
@@ -0,0 +1,7 @@
+package com.robam.ventilator.fragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
